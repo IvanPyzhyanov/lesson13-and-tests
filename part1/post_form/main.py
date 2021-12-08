a54baba58e32982da3ce316f8947795a8a21365d
IvanPyzhyanov <piv85@mail.ru>
2021-12-08 11:42:49 +0300

    post_form ready

--- part1/post_form/main.py
+++ part1/post_form/main.py
@@ -8,16 +8,28 @@
 
 import os
 from pathlib import Path
-from flask import Flask
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
 
 @app.route("/settings", methods=["GET", "POST"])
 def update_settings():
-     # TODO Напишите Ваш код здесь
-     pass
+     if request.method == "POST":
+         name = request.form.get("project_name")
+         tagline = request.form.get("project_tagline")
+         input_data = {"project_name": name, "project_tagline": tagline}
+         with open("settings.json", "w", encoding="UTF-8") as file:
+             json.dump(input_data, file, ensure_ascii=False)
+         with open("settings.json", "r", encoding="UTF-8") as file:
+             data_json = json.load(file)
+         return render_template("settings.html", settings=data_json)
+     else:
+         with open("settings.json", "r", encoding="UTF-8") as file:
+            data_json = json.load(file)
+         return render_template("settings.html", settings=data_json)
 
-if __name__ == "__main__":
-    os.chdir(Path(os.path.abspath(__file__)).parent)
-    app.run()
+
+# if __name__ == "__main__":
+#     os.chdir(Path(os.path.abspath(__file__)).parent)
+app.run()
