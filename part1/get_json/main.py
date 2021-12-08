fdce08a535d55b0cd6dcc57fede1d7d2baae1fef
IvanPyzhyanov <piv85@mail.ru>
2021-12-08 11:49:23 +0300

    get_json ready

--- part1/get_json/main.py
+++ part1/get_json/main.py
@@ -3,19 +3,19 @@
 
  
 
-import os
-from pathlib import Path
-from flask import Flask, jsonify
-import json
+# import os
+# from pathlib import Path
+from flask import Flask, jsonify, json, request
+# import json
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 @app.route("/tours", methods=["GET"])
 def get_tours():
-    # TODO Напишите Ваш код здесь
-    pass
+    with open("tours.json", "r", encoding="UTF-8") as file:
+        json_data = json.load(file)
+    return jsonify(json_data)
 
-
-if __name__ == "__main__":
-    os.chdir(Path(os.path.abspath(__file__)).parent)
-    app.run()
+# if __name__ == "__main__":
+#     os.chdir(Path(os.path.abspath(__file__)).parent)
+app.run()
